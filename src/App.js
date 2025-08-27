28103dd0b89592ddc8845a057cb45766903b3f3f
KT147 <kevin.tammm@gmail.com>
2025-08-27 13:14:37 +0300

    action creator thunk

--- src/App.js
+++ src/App.js
@@ -1,27 +1,82 @@
-import { useEffect } from 'react';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { Fragment, useEffect } from "react";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector, useDispatch } from "react-redux";
+import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-slice";
+
+let isInitial = true
 
 function App() {
+  const showcart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
 
-  const showcart = useSelector(state => state.ui.cartIsVisible)
-  const cart = useSelector(state => state.cart)
+  const dispatch = useDispatch()
+
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    fetch("https://database-982fe-default-rtdb.europe-west1.firebasedatabase.app/cart.json", {
-      method: "PUT",
-      body: JSON.stringify(cart)
-    })
+    // const sendCartData = async () => {
+      // dispatch(
+      //   uiActions.showNotification({
+      //     status: "pending",
+      //     title: "Sending",
+      //     message: "Sending cart data!",
+      //   })
+      // );
+      // const response = await fetch(
+      //   "https://database-982fe-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+      //   {
+      //     method: "PUT",
+      //     body: JSON.stringify(cart),
+      //   }
+      // );
 
-  }, [cart]);
+      // if (!response.ok) {
+      //   throw new Error("Sendind cart data failed");
+      // }
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: "success",
+    //       title: "Success",
+    //       message: "Sent cart data successfully!",
+    //     })
+    //   );
+    // };
+
+    if (isInitial) {
+      isInitial = false;
+      return
+    }
+
+    dispatch(sendCartData(cart))
+
+    // sendCartData().catch((error) => {
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: "error",
+    //       title: "Error",
+    //       message: "Sending cart data failed!",
+    //     })
+    //   );
+    // });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showcart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showcart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -10,18 +11,18 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
-      state.totalQuantity++
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
-          name: newItem.title
+          name: newItem.title,
         });
       } else {
-        existingItem.quantity++
-        existingItem.totalPrice = existingItem.totalPrice + newItem.price
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
     removeToCart(state, action) {
@@ -32,15 +33,59 @@ const cartSlice = createSlice({
 
       state.totalQuantity--;
       if (existingItem.quantity === 1) {
-          state.items = state.items.filter(item => item.id !== id);
+        state.items = state.items.filter((item) => item.id !== id);
       } else {
-          existingItem.quantity--;
-          existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
-  },
+    },
   },
 });
 
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending",
+        message: "Sending cart data!",
+      })
+    );
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://database-982fe-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sendind cart data failed");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
+
 export const cartActions = cartSlice.actions;
 
-export default cartSlice
+export default cartSlice;
